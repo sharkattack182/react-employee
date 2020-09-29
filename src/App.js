0998b86cd2bd6261d648e1c59f9c4d79effe377b
Matthew Courtney <matthewdc182@gmail.com>
2020-09-29 00:45:17 -0400

    added in react files also made a component folder with files for header searchbar and results table also added in bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/header';
+import Search from './components/search';
+import Table from './components/table';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Search />
+    <Table />
+    </div>
+  );
+}
+
+export default App;
